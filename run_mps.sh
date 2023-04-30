4df847139117c158e5e83a37264dd0fc2a4c11b0
Neil Kim Nielsen <neilkimn@protonmail.com>
2023-04-30 19:38:20 +0200

    ImageNet experiments

--- run_mps.sh
+++ run_mps.sh
@@ -15,23 +15,29 @@ sudo nvidia-smi -i 0 -c EXCLUSIVE_PROCESS
 sudo nvidia-cuda-mps-control -d
 
 # run some stuff ...
-./train_multiple_mps.sh
-#LOG_DIR="/home/kafka/repos/thesis/logs/single_runs/"
+#./train_multiple_mps.sh
+./train_multiple.sh
+#LOG_DIR="/home/kafka/repos/thesis/logs_2/single_runs/"
 #DEBUG_DIR="/home/kafka/repos/thesis/debug_data/"
 #CUDA_VISIBLE_DEVICES=0
 #
 #sudo sh -c "/bin/echo 3 > /proc/sys/vm/drop_caches"
-#CUDA_MPS_ACTIVE_THREAD_PERCENTAGE=50
-#/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 10 \
-#    --seed 1234 --arch resnet18 --pretrained \
-#    --batch-size 80 --training-workers 4 --validation-workers 4 \
-#    --log_path "${LOG_DIR}resnet18_bs80" &
 #
-#CUDA_MPS_ACTIVE_THREAD_PERCENTAGE=50
-#/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 10 \
-#    --seed 1234 --arch resnet34 --pretrained \
-#    --batch-size 80 --training-workers 4 --validation-workers 4 \
-#    --log_path "${LOG_DIR}resnet34_bs80"
+##CUDA_MPS_ACTIVE_THREAD_PERCENTAGE=50
+#/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 11 \
+#    --seed 1234 --arch resnet18 --pretrained \
+#    --batch-size 50 --training-workers 8 --validation-workers 2 \
+#    --log_path "${LOG_DIR}resnet18_bs50" &
+#
+#/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 11 \
+#    --seed 1234 --arch resnet18 --pretrained \
+#    --batch-size 50 --training-workers 8 --validation-workers 2 \
+#    --log_path "${LOG_DIR}resnet18_bs50" &
+#
+#/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 11 \
+#    --seed 1234 --arch resnet18 --pretrained \
+#    --batch-size 50 --training-workers 8 --validation-workers 2 \
+#    --log_path "${LOG_DIR}resnet18_bs50"
 
 # shut down MPS control daemon
 sudo echo quit | sudo nvidia-cuda-mps-control
--- train_multiple.sh
+++ train_multiple.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-LOG_DIR="/home/kafka/repos/thesis/logs/"
+LOG_DIR="/home/kafka/repos/thesis/logs_imagenet/"
 DEBUG_DIR="/home/kafka/repos/thesis/debug_data/"
 CUDA_VISIBLE_DEVICES=0
 
 sudo sh -c "/bin/echo 3 > /proc/sys/vm/drop_caches"
-/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_multiple.py --seed 1234 \
-    --arch resnet18 resnet18 resnet34 --epochs 10 --pretrained true true true \
-    --num-processes 3 --batch-size 50 --training-workers 8 --validation-workers 4 \
-    --record_first_batch_time \
+/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_multiple_imagenet.py --seed 1234 \
+    --arch resnet18 resnet18 --epochs 2 --pretrained true true \
+    --num-processes 2 --batch-size 128 --training-workers 8 --validation-workers 2 \
     --log_dir "${LOG_DIR}queues" --record_first_batch_time
     #--debug_data_dir "${DEBUG_DIR}train_queues_debug"
