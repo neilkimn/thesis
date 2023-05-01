0d9042d823d264ffdd5a1e20cde24d62ab0ea5b4
Neil Kim Nielsen <neilkimn@protonmail.com>
2023-05-01 16:11:32 +0200

    Add eBPF tracing for read calls

--- train_multiple.sh
+++ train_multiple.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-LOG_DIR="/home/kafka/repos/thesis/logs/"
+LOG_DIR="/home/kafka/repos/thesis/logs_bpf/"
 DEBUG_DIR="/home/kafka/repos/thesis/debug_data/"
 CUDA_VISIBLE_DEVICES=0
 
 sudo sh -c "/bin/echo 3 > /proc/sys/vm/drop_caches"
 /home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_multiple.py --seed 1234 \
-    --arch resnet18 resnet18 resnet34 --epochs 10 --pretrained true true true \
-    --num-processes 3 --batch-size 50 --training-workers 8 --validation-workers 4 \
-    --record_first_batch_time \
-    --log_dir "${LOG_DIR}queues" --record_first_batch_time
+    --arch resnet18 resnet18 --epochs 1 --pretrained true true \
+    --num-processes 2 --batch-size 50 --training-workers 1 --validation-workers 1 \
+    --record_first_batch_time --log_dir "${LOG_DIR}queues" &
     #--debug_data_dir "${DEBUG_DIR}train_queues_debug"
+python_pid=$!
+
+echo "Python process started with PID $python_pid"
+
+sudo bpftrace bpf_traces/bpf_read.bt -o logs_bpf/bpf_traces/trace_read_shared.out &
--- train_single.sh
+++ train_single.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-LOG_DIR="/home/kafka/repos/thesis/logs/single_runs/"
+LOG_DIR="/home/kafka/repos/thesis/logs_bpf/single_runs/"
 DEBUG_DIR="/home/kafka/repos/thesis/debug_data/"
 CUDA_VISIBLE_DEVICES=0
 
 sudo sh -c "/bin/echo 3 > /proc/sys/vm/drop_caches"
-/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 10 \
+/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 1 \
     --seed 1234 --arch resnet18 --pretrained \
-    --batch-size 50 --training-workers 4 --validation-workers 2 \
+    --batch-size 50 --training-workers 1 --validation-workers 1 \
     --log_path "${LOG_DIR}resnet18_bs50" &
 
-/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 10 \
+/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 1 \
     --seed 1234 --arch resnet18 --pretrained \
-    --batch-size 50 --training-workers 4 --validation-workers 2 \
+    --batch-size 50 --training-workers 1 --validation-workers 1 \
     --log_path "${LOG_DIR}resnet18_bs50" &
 
-/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 10 \
-    --seed 1234 --arch resnet34 --pretrained \
-    --batch-size 50 --training-workers 4 --validation-workers 2 \
-    --log_path "${LOG_DIR}resnet34_bs50"
+sudo bpftrace bpf_traces/bpf_read.bt -o logs_bpf/bpf_traces/trace_read_single.out &
 
 #/home/kafka/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py --epochs 10 \
 #    --seed 1234 --arch resnet34 --pretrained \
