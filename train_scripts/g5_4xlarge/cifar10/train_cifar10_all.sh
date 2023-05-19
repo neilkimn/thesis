e0382e2e548a6198b5342a1352324a8ce2600d8b
Ubuntu <ubuntu@ip-172-31-10-63.eu-north-1.compute.internal>
2023-05-19 17:24:11 +0000

    g5.4xlarge cifar10

---
+++ train_scripts/g5_4xlarge/cifar10/train_cifar10_all.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Naive
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_1x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_2x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_3x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_4x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_5x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_6x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_7x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_8x.sh
+
+# Naive MPS
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_1x.sh
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_2x.sh
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_3x.sh
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_4x.sh
+
+# Shared
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_1x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_2x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_3x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_4x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_5x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_6x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_7x.sh
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_8x.sh
+
+# Shared MPS
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_1x.sh
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_2x.sh
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_3x.sh
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_4x.sh
+
+# Naive DALI
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_1x.sh --use-dali
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_2x.sh --use-dali
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_3x.sh --use-dali
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_single_4x.sh --use-dali
+
+# Shared DALI
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_1x.sh --use-dali
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_2x.sh --use-dali
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_3x.sh --use-dali
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10/train_shared_4x.sh --use-dali
---
+++ train_scripts/g5_4xlarge/cifar10/train_single_4x.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+LOG_DIR="/home/ubuntu/repos/thesis/logs_all/single_runs"
+DEBUG_DIR="/home/ubuntu/repos/thesis/debug_data/"
+CUDA_VISIBLE_DEVICES=0
+
+MODEL="resnet18"
+BATCH_SIZE=128
+DATASET="cifar10"
+MODEL_NAME="${MODEL}_bs_${BATCH_SIZE}"
+EPOCHS=11
+WORKERS=16
+
+sleep 1
+if [[ ! -e ${LOG_DIR}/${DATASET}/${MODEL_NAME} ]]; then
+    mkdir -p ${LOG_DIR}/${DATASET}/${MODEL_NAME}
+fi
+
+sudo sh -c "/bin/echo 3 > /proc/sys/vm/drop_caches"
+
+/home/ubuntu/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py \
+    --log-interval 10 --epochs $EPOCHS --arch "resnet18" --pretrained --dataset $DATASET \
+    --batch-size $BATCH_SIZE --training-workers $WORKERS --validation-workers 1 \
+    --log_path "${LOG_DIR}/${DATASET}/${MODEL_NAME}" $1 &
+
+/home/ubuntu/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py \
+    --log-interval 10 --epochs $EPOCHS --arch "resnet18" --pretrained --dataset $DATASET \
+    --batch-size $BATCH_SIZE --training-workers $WORKERS --validation-workers 1 \
+    --log_path "${LOG_DIR}/${DATASET}/${MODEL_NAME}" $1 &
+
+/home/ubuntu/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py \
+    --log-interval 10 --epochs $EPOCHS --arch "resnet18" --pretrained --dataset $DATASET \
+    --batch-size $BATCH_SIZE --training-workers $WORKERS --validation-workers 1 \
+    --log_path "${LOG_DIR}/${DATASET}/${MODEL_NAME}" $1 &
+
+/home/ubuntu/miniconda3/envs/thesis/bin/python src/shared_queues/train_single.py \
+    --log-interval 10 --epochs $EPOCHS --arch "resnet18" --pretrained --dataset $DATASET \
+    --batch-size $BATCH_SIZE --training-workers $WORKERS --validation-workers 1 \
+    --log_path "${LOG_DIR}/${DATASET}/${MODEL_NAME}" $1 &
+
+    #--debug_data_dir "${DEBUG_DIR}train_single_debug" &
+
+training_main_proc=$!
+
+echo "Starting training process with PID $training_main_proc"
+
+sleep 1
+
+mpstat 1 -P 0-15 > ${LOG_DIR}/${DATASET}/${MODEL_NAME}/pid_${training_main_proc}_cpu.out &
+trace_cpu_pid=$!
+
+nvidia-smi pmon -s um -o DT -f ${LOG_DIR}/${DATASET}/${MODEL_NAME}/pid_${training_main_proc}_gpu.out &
+trace_gpu_pid=$!
+
+iostat 1 -m -t nvme0n1 > ${LOG_DIR}/${DATASET}/${MODEL_NAME}/pid_${training_main_proc}_io.out &
+trace_io_pid=$!
+
+echo "Started mpstat (PID: $trace_cpu_pid), iostat (PID: $trace_io_pid) and nvidia-smi (PID: $trace_gpu_pid)"
+
+while kill -0 "$training_main_proc"; do
+    sleep 5
+done
+
+sleep 20
+
+kill $trace_cpu_pid
+kill $trace_gpu_pid
+kill $trace_io_pid
