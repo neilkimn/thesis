77bd8b50df6e06167a57ea2cbe22fa5a27ce925b
Ubuntu <ubuntu@ip-172-31-10-63.eu-north-1.compute.internal>
2023-05-25 21:12:19 +0000

    g5.4xlarge cifar10 mixed

---
+++ train_scripts/g5_4xlarge/cifar10_mixed/train_cifar10_all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Naive
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10_mixed/train_naive_mixed_3x.sh
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10_mixed/train_naive_mixed_3x_at_once.sh
+
+# Naive MPS
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10_mixed/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10_mixed/train_naive_mixed_3x_at_once.sh
+
+# Shared
+#/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10_mixed/train_shared_mixed_3x_mps.sh
+
+# Shared MPS
+/home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10_mixed/run_mps.sh /home/ubuntu/repos/thesis/train_scripts/g5_4xlarge/cifar10_mixed/train_shared_mixed_3x_mps.sh
+
+# Shared MPS weights
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10_mixed/run_mps.sh /home/kafka/repos/thesis/train_scripts/desktop/cifar10_mixed/train_shared_mixed_2x_weights.sh
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10_mixed/run_mps.sh /home/kafka/repos/thesis/train_scripts/desktop/cifar10_mixed/train_shared_mixed_3x_weights.sh
+
+# Naive DALI
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_single_1x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_single_2x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_single_3x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_single_4x.sh --use-dali
+
+# Shared DALI
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_shared_1x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_shared_2x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_shared_3x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_shared_4x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_shared_5x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_shared_6x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_shared_7x.sh --use-dali
+#/home/kafka/repos/thesis/train_scripts/desktop/cifar10/train_shared_8x.sh --use-dali
